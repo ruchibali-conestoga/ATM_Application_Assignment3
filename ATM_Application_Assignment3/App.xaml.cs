2aaa63fc3f629c62cf6659292ce0bd74b12df56e
Ruchi Bali <Rbali1438@conestogac.on.ca>
2024-11-20 16:43:37 -0500

    Add project files.

---
+++ ATM_Application_Assignment3/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ATM_Application_Assignment3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
